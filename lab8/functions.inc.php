b92c8cd4c5f3be23ac2655060943d4d01ec1fbf2
yunlicangliu <18302010023@fudan.edu.cn>
2019-05-15 23:23:22 +0800

    lab8

--- lab8/functions.inc.php
+++ lab8/functions.inc.php
@@ -1,8 +1,16 @@
 <?php
+function outputOrderRow($file, $title, $quantity, $price) {
+    $total = $quantity * $price;
+    $price = number_format($price, 2);
+    $total = number_format($total, 2);
 
-    function outputOrderRow($file, $title, $quantity, $price) {
-        echo "<tr>";
-        //TODO
-        echo "</tr>";
-    }
+
+    echo "<tr>";
+    echo "<td><img src='images/books/tinysquare/$file'></td>";
+    echo "<td class=\"mdl-data-table__cell--non-numeric\">$title</td>";
+    echo "<td>$quantity</td>";
+    echo "<td>$$price</td>";
+    echo "<td>$$total</td>";
+    echo "</tr>";
+}
 ?>
