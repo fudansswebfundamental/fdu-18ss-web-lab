6abb779effc4a61ae61485b98ba45fb30139f480
YangYunteng <18302010022@fudan.edu.cn>
2019-05-22 21:42:59 +0800

    lab9提交

--- lab9/Lab09.php
+++ lab9/Lab09.php
@@ -2,8 +2,6 @@
 include 'travel-data.inc.php';
 
 
-
-
 ?>
 
 <!DOCTYPE html>
@@ -13,85 +11,105 @@ include 'travel-data.inc.php';
     <meta charset="utf-8">
     <title>Chapter 12</title>
 
-      <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
 
-    <link rel="stylesheet" href="css/bootstrap.min.css" />
-    
-    
-    <link rel="stylesheet" href="css/bootstrap-theme.css" />
-    <link rel="stylesheet" href="css/captions.css" />
+    <link rel="stylesheet" href="css/bootstrap.min.css"/>
+
+
+    <link rel="stylesheet" href="css/bootstrap-theme.css"/>
+    <link rel="stylesheet" href="css/captions.css"/>
 
 </head>
 
 <body>
-    <?php include 'header.inc.php'; ?>
-    
+<?php include 'header.inc.php'; ?>
 
 
-    <!-- Page Content -->
-    <main class="container">
-     
-        
-        <div class="btn-group countryButtons" role="group" aria-label="...">
-              <a role="button" class="btn btn-default" href="list.php">All</a>
-           
-              <?php   
-              /* you will need to fill this place with appropriate PHP */
-              /* hint: use array and loop */
-              //the next line is an example
-              //<a href="list.php?country=Canada" role="button" class="btn btn-default"> Canada</a> 
+<!-- Page Content -->
+<main class="container">
 
 
-              ?>
-                     
-        </div>               
-           
-        
+    <div class="btn-group countryButtons" role="group" aria-label="...">
+        <a role="button" class="btn btn-default" href="list.php">All</a>
+        <?php
+        /* for ($i = 0; $i < count($countries); $i++) {
+             echo "<a role='button' class='btn btn-default' href='list.php' >$countries[$i]</a>";
+         }*/
+        foreach ($countries as $key => $value) {
+            echo "<a role='button' class='btn btn-default' href='list.php'>$value</a>";
+        }
+        /* you will need to fill this place with appropriate PHP */
+        /* hint: use array and loop */
+        //the next line is an example
+        //<a href="list.php?country=Canada" role="button" class="btn btn-default"> Canada</a>
+        ?>
 
-		<ul class="caption-style-2">
-         
-          <?php   
-          /* you will need to fill this place with appropriate PHP */ 
-          /* hint: use array and loop */
-          //the next lines are one of the elements
-          //you need to fill the elements with data provided
+    </div>
 
 
-   //        <li>
-   //              <a href="detail.php?id=22" class="img-responsive">
-   //              	<img src="images/square/6114850721.jpg" alt="View of Cologne">
-   //              	<div class="caption">
-   //              		<div class="blur">
-   //              		</div>
-   //              		<div class="caption-text">
-   //              			<h1>View of Cologne</h1>
-   //              		</div>
-   //              	</div>
-   //              </a>
-			// </li>
-          ?>
+    <ul class="caption-style-2">
 
-       </ul>       
+        <?php
+        /* you will need to fill this place with appropriate PHP */
+        /* hint: use array and loop */
+        //the next lines are one of the elements
+        //you need to fill the elements with data provided
+        foreach ($images as $id => $image) {
+            $href1="detail.php？id=".$id;
+            $img_src="images/square/".$image["path"];
+            $alt=$image["title"];
+            echo "<li>";
+            echo "<a class='img-responsive' href=$href1 >";
+            echo "<img src=$img_src alt=$alt>";
+            echo "<div class='caption'>";
+            echo "<div class='blur'>";
+            echo "</div>";
+            echo "<div class='caption-text'>
+<h1>$alt</h1>
+</div>";
+            echo "</div>";
+            echo "</a>";
+            echo "</li>";
+        }
 
-      
-    </main>
-    
-    <footer>
-        <div class="container-fluid">
-                    <div class="row final">
-                <p>Copyright &copy; 2017 Creative Commons ShareAlike</p>
-                <p><a href="#">Home</a> / <a href="#">About</a> / <a href="#">Contact</a> / <a href="#">Browse</a></p>
-            </div>            
+        //        <li>
+        //              <a href="detail.php?id=22" class="img-responsive">
+        //              	<img src="images/square/6114850721.jpg" alt="View of Cologne">
+        //              	<div class="caption">
+        //              		<div class="blur">
+        //              		</div>
+        //              		<div class="caption-text">
+        //              			<h1>View of Cologne</h1>
+        //              		</div>
+        //              	</div>
+        //              </a>
+        // </li>
+        ?>
+
+    </ul>
+
+
+</main>
+
+<footer>
+    <div class="container-fluid">
+        <div class="row final">
+            <p>Copyright &copy; 2017 Creative Commons ShareAlike</p>
+            <p><a href="#">Home</a> / <a href="#">About</a> / <a href="#">Contact</a> / <a href="#">Browse</a></p>
         </div>
-        
-
-    </footer>
+    </div>
 
 
-        <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+</footer>
+
+
+<script src="https://code.jquery.com/jquery-2.2.4.min.js"
+        integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
+        integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
+        crossorigin="anonymous"></script>
 </body>
 
 </html>
